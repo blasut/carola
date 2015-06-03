4222cb387de8a46e9e6d6f1a903b4e0a7b4332ae
oklaridentitet <oklaridentitet@gmail.com>
2015-06-03 22:06:26 +0200

    rename currency to currency pair

--- carola.lisp
+++ carola.lisp
@@ -17,7 +17,7 @@
     (setf (flexi-streams:flexi-stream-external-format stream) :utf-8)
     (json:decode-json stream)))
 
-(defclass currency ()
+(defclass currency-pair ()
   ((from
     :initarg :from
     :initform (error "Must supply a value for :from")
@@ -32,22 +32,27 @@
    (url-name
     :accessor url-name
     :documentation "The name used in the URL for requests."))
-  (:documentation "This class describes a currency for the exchange. A currency always have a from and to currency, for example. BTC to XMR."))
+  (:documentation "This class describes a currency-pair for the exchange. A currency-pair always have a from and to currency, for example. BTC to XMR."))
 
-(defmethod initialize-instance :after ((currency currency) &key)
-  (let ((from (slot-value currency 'from))
-        (to (slot-value currency 'to)))
-    (setf (slot-value currency 'api-name) (string-upcase (str "+" from "-" to "+")))
-    (setf (slot-value currency 'url-name) (string-upcase (str from "_" to)))))
+(defmethod initialize-instance :after ((currency-pair currency-pair) &key)
+  (let ((from (slot-value currency-pair 'from))
+        (to (slot-value currency-pair 'to)))
+    (setf (slot-value currency-pair 'api-name) (string-upcase (str "+" from "-" to "+")))
+    (setf (slot-value currency-pair 'url-name) (string-upcase (str from "_" to)))))
 
-(defmethod get-latest-ticker ((currency currency))
-  (with-slots (api-name) currency
+(defgeneric get-latest-ticker (currency-pair))
+(defgeneric get-trade-history (currency-pair))
+(defgeneric get-order-book (currency-pair))
+
+
+(defmethod get-latest-ticker ((currency-pair currency-pair))
+  (with-slots (api-name) currency-pair
     (remove-if-not #'(lambda (x) (string= (string (first x)) api-name)) (make-request "returnTicker"))))
 
-(defmethod get-order-book ((currency currency))
-  (with-slots (url-name) currency
+(defmethod get-order-book ((currency-pair currency-pair))
+  (with-slots (url-name) currency-pair
     (make-request (str "returnOrderBook&currencyPair=" url-name "&depth=50"))))
 
-(defmethod get-trade-history ((currency currency))
-  (with-slots (url-name) currency
+(defmethod get-trade-history ((currency-pair currency-pair))
+  (with-slots (url-name) currency-pair
     (make-request (str "returnTradeHistory&currencyPair=" url-name))))
