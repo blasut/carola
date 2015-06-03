f576066817952a37ec7a5d96bf9428c40f8d96c0
oklaridentitet <oklaridentitet@gmail.com>
2015-06-03 15:18:06 +0200

    get latest ticker

--- carola.lisp
+++ carola.lisp
@@ -2,5 +2,22 @@
 
 (in-package #:carola)
 
+(ql:quickload 'drakma)
+
+(defmacro str (&rest body)
+  `(concatenate 'string ,@body))
+
+(setf drakma:*header-stream* *standard-output*)
+
+(defparameter *command-url* "https://poloniex.com/public?command=")
+
+(defun get-latest-ticker ()
+  (let* ((ticker (str *command-url* "returnTicker"))
+        (stream (drakma:http-request ticker :want-stream t)))
+    (setf (flexi-streams:flexi-stream-external-format stream) :utf-8)
+    (json:decode-json stream)))
+
+
+
 ;;; "carola" goes here. Hacks and glory await!
 
