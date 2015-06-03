148f74edb85836b607e808e91ad2ea0c523ac3ea
oklaridentitet <oklaridentitet@gmail.com>
2015-06-03 15:44:21 +0200

    get ticker for currency

--- carola.lisp
+++ carola.lisp
@@ -17,6 +17,15 @@
     (setf (flexi-streams:flexi-stream-external-format stream) :utf-8)
     (json:decode-json stream)))
 
+;; Expects: Two strings in the format: XXX, XXX+
+;;          Both strings should be 3 or more characters long.
+;; Example: BTC, LTC
+(defun get-latest-ticker-for-currency (from to)
+  (let ((ticker-name (string-upcase (str "+" from "-" to "+")))
+        (ticker (get-latest-ticker)))
+    (remove-if-not #'(lambda (x) (string= (string (first x)) ticker-name)) ticker)))
+
+
 
 
 ;;; "carola" goes here. Hacks and glory await!
