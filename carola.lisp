5b65b94b792fa27cca9bce8446f8f476dc2f5af3
oklaridentitet <oklaridentitet@gmail.com>
2015-06-03 15:57:55 +0200

    order book for currency

--- carola.lisp
+++ carola.lisp
@@ -21,12 +21,13 @@
 ;;          Both strings should be 3 or more characters long.
 ;; Example: BTC, LTC
 (defun get-latest-ticker-for-currency (from to)
-  (let ((ticker-name (string-upcase (str "+" from "-" to "+")))
+  (let ((currency-name (string-upcase (str "+" from "-" to "+")))
         (ticker (get-latest-ticker)))
-    (remove-if-not #'(lambda (x) (string= (string (first x)) ticker-name)) ticker)))
+    (remove-if-not #'(lambda (x) (string= (string (first x)) currency-name)) ticker)))
 
-
-
-
-;;; "carola" goes here. Hacks and glory await!
-
+(defun get-order-book-for-currency (from to)
+  (let* ((currency-name (string-upcase (str from "_" to)))
+         (ticker (str *command-url* "returnOrderBook&currencyPair=" currency-name "&depth=50"))
+           (stream (drakma:http-request ticker :want-stream t)))
+      (setf (flexi-streams:flexi-stream-external-format stream) :utf-8)
+      (json:decode-json stream)))
