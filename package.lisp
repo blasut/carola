6e619f563ae1c4f3b9eae7bdc2e948d3463bd29f
blasut <blasut937@gmail.com>
2015-09-27 11:14:29 +0200

    export all public methods

--- package.lisp
+++ package.lisp
@@ -1,11 +1,37 @@
 (defpackage #:carola
   (:use #:cl
         #:drakma)
-  (:export :get-balances
-           :make-currency
-           :get-loan-orders
+  (:export :make-currency
            :make-currency-pair
+           :get-all-order-books
+           :get-balances
+           :get-complete-balances
+           :get-deposit-addresses
+           :get-deposit-and-withdrawals
+           :get-all-open-orders
+           :get-all-trade-histories
+           :cancel-order
+           :move-order
+           :get-available-account-balances
+           :get-tradable-balances
+           :get-margin-account-summary
+           :get-margin-positions
+           :cancel-loan-order
+           :get-open-loan-orders
+           :get-active-loans
+           :toggle-auto-renew
+           :get-loan-orders
+           :withdraw
+           :transfer
+           :create-loan-offer
+           :generate-new-address
            :get-latest-ticker
            :get-order-book
-           :get-trade-history))
-
+           :get-trade-history
+           :get-open-orders
+           :buy
+           :sell
+           :margin-buy
+           :margin-sell
+           :get-margin-position
+           :close-margin-position))
